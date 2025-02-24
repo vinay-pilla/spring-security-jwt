eb4c903b6bb1384d9e41ac71deb572646d060fb8
vinaypilla27 <vinaypilla27@gmail.com>
2025-02-24 22:12:50 +0530

    added comment in root class

--- spring-security_jwt/src/main/java/com/pilla/security/SpringSecurityJwtApplication.java
+++ spring-security_jwt/src/main/java/com/pilla/security/SpringSecurityJwtApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
-
+	//test 01
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
